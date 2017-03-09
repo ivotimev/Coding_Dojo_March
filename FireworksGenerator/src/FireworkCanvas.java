9c02a16f524eec6d9e4dd59cc06ee8b157d4a708
Shakrazam <ivo.timev@gmail.com>
2017-03-09 19:26:15 +0000

    stuff

---
+++ FireworksGenerator/src/FireworkCanvas.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Dell on 09/03/2017.
+ */
+public class FireworkCanvas extends JFrame{
+    public FireworkCanvas() throws HeadlessException {
+        super("Fireworks Generator");
+
+    }
+}
